7f8d68946bfc76df6bbe9c63d11ba6d69c471093
atomizer3 <atomizer3@gmail.com>
2014-03-29 19:24:25 +0100

    test

---
+++ Test project/src/com/test/test_project/Test.java
@@ -0,0 +1,11 @@
+package com.test.test_project;
+
+/**
+ * Created by Tomek on 2014-03-29.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Yo mama");
+
+    }
+}
