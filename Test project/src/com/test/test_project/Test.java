d22787ccdac67b6f025ecf0d8ca62c81f9d558c6
atomizer3 <atomizer3@gmail.com>
2014-03-29 19:36:26 +0100

    inteli commit

--- Test project/src/com/test/test_project/Test.java
+++ Test project/src/com/test/test_project/Test.java
@@ -3,9 +3,13 @@ package com.test.test_project;
 /**
  * Created by Tomek on 2014-03-29.
  */
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("Yo mama");
         //zmiana jakaś coś sobie napisałem
+    }
+    public void local_funcion(){
+        System.out.println("pisze cos tylko lokalnie");
     }
 }
